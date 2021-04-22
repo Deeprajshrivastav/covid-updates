a21c0470a2edfd0ab5dcfbc6c576882af7836b6e
Deep Raj <65335438+Deeprajshrivastav@users.noreply.github.com>
2021-04-22 21:39:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
+    data = states.getdata('Total')
+    data = list(data.values())
+    confirmed_case = str(data[1] + data[2] + data[3])
+    active_case = str(data[1])
+    recovered_case = str(data[2])
+    deaths = str(data[3])
+    icfc = "Confirmed case: " + confirmed_case
+    iatc = "Active case: " + active_case
+    irvc = "Recovered: " + recovered_case
+    idth = "Deaths: " + deaths
+    return render_template('index.html', icfc=icfc, iatc=iatc, irvc=irvc, idth=idth)
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -37,9 +46,8 @@ def search():
     atc = "Active case: " + active_case
     rvc = "Recovered: " + recovered_case
     dth = "Deaths: " + deaths
-    return render_template('index.html',stat=state[0], cfc=cfc, atc=atc, rvc=rvc, dth=dth, icfc=icfc, iatc=iatc, irvc=irvc, idth=idth)
+    return render_template('index.html', stat=state[0], cfc=cfc, atc=atc, rvc=rvc, dth=dth, icfc=icfc, iatc=iatc, irvc=irvc, idth=idth)
 
 
 if __name__ == '__main__':
-    
     app.run(debug=True)
